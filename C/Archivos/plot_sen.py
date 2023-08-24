24604e364579b51373897bcfd532031298b53e17
Valentín Lunati <lunativ@gmail.com>
2023-08-24 00:21:47 -0300

    Update plot_sen.py
    add name of file from args

--- C/Archivos/plot_sen.py
+++ C/Archivos/plot_sen.py
@@ -1,13 +1,19 @@
 import numpy as np
+import argparse
 import matplotlib.pyplot as plt
+# Define los argumentos del script
+parser = argparse.ArgumentParser(description="Genera una gráfica a partir de un archivo de texto")
+parser.add_argument("filename", type=str, help="Nombre del archivo de texto")
+args = parser.parse_args()
+
 
 # Cargar los datos del archivo
-senal = np.loadtxt('senal.txt')
+senal = np.loadtxt(args.filename)
 
 # Graficar la señal
 plt.plot(senal[0:150])
 plt.xlabel('Muestras')
 plt.ylabel('Amplitud')
-plt.title('Señal')
+plt.title(args.filename)
 plt.grid(True)
 plt.show()
